5a15c8055fe11333ed9e71c701a9be6e2aca2fcc
종현 나 <auma-@naver.com>
2021-04-25 22:28:04 +0900

    일단 출시

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:sleep_manager/test.dart';
 import 'package:sleep_manager/utils/uidata.dart';
+import 'package:sleep_manager/widget/cycle_item.dart';
 
 bool darkmode = true;
 void main() {
@@ -80,15 +81,22 @@ class _MyHomePageState extends State<MyHomePage>
         ),
       ),
       body: TabBarView(
-        children: [screen1(), screen1(), screen1()],
+        children: [screen1(), notImplemented(), notImplemented()],
         controller: _tabController,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (mounted) setState(() {});
+        },
+        child: Icon(Icons.refresh),
       ),
     );
   }
 
   Widget screen1() {
     DateTime _tempTime = DateTime.now();
-    String _formattedDate = DateFormat('a kk:mm').format(_tempTime);
+    String _formattedDate =
+        CycleItem.timeEnToKo(DateFormat('a hh:mm').format(_tempTime));
 
     return Container(
       decoration: BoxDecoration(color: Theme.of(context).backgroundColor),
@@ -108,58 +116,18 @@ class _MyHomePageState extends State<MyHomePage>
             ),
           ),
           Expanded(
-            child: ListView(
-              children: [
-                Card(
-                  child: Container(
-                    padding: EdgeInsets.only(
-                        right: 20, left: 20, top: 20, bottom: 20),
-                    child: Row(
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              '1',
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.w800),
-                            ),
-                            Text(
-                              '사이클',
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.w800),
-                            )
-                          ],
-                        ),
-                        // VerticalDivider(
-                        //   width: 2,
-                        //   thickness: 2,
-                        //   color: Colors.white,
-                        // ),
-                        SizedBox(width: 20),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '21/4/24 오후 11:52',
-                                style: TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.w800),
-                              ),
-                              Text(
-                                '낮잠 03h14m',
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w800),
-                              )
-                            ],
-                          ),
-                        ),
-                        Icon(Icons.alarm),
-                      ],
-                    ),
-                  ),
-                )
-              ],
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                DateTime wakeUpTime = _tempTime.add(
+                    Duration(hours: 1, minutes: 44) +
+                        Duration(hours: 1, minutes: 30) * index);
+                return CycleItem(
+                  cycle: index + 1,
+                  nowTime: _tempTime,
+                  wakeUpTime: wakeUpTime,
+                );
+              },
             ),
           ),
         ],
@@ -172,4 +140,17 @@ class _MyHomePageState extends State<MyHomePage>
           Color color,
           FontWeight fontWeight = FontWeight.w600}) =>
       Text(text, style: TextStyle(color: color, fontWeight: fontWeight));
+
+  Widget notImplemented() => Container(
+        color: Theme.of(context).backgroundColor,
+        child: Center(
+          child: Text(
+            '준비중',
+            style: TextStyle(
+                color: Colors.redAccent,
+                fontSize: 54,
+                fontWeight: FontWeight.w800),
+          ),
+        ),
+      );
 }
